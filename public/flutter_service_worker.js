8eb791a0aa0d85e2fdc5b45ed41ca923666dfab7
Mahboob Hasan <mahboobhasan@Mahboobs-MacBook-Pro.local>
2026-08-26 18:29:58 +0530

    Verify and optimize Practice Screen 1 layout, custom vector graphics, colors and proportions

--- public/flutter_service_worker.js
+++ public/flutter_service_worker.js
@@ -3,7 +3,7 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "967021531f208525a6781a616e7d745f",
+const RESOURCES = {"flutter_bootstrap.js": "efbf06c65d12ce100db9386ef760329a",
 "version.json": "7e6bcb3a73bf6b9b05e77f2ef1b8391d",
 "index.html": "8d30659e577350f2771dc0b659d700da",
 "/": "8d30659e577350f2771dc0b659d700da",
