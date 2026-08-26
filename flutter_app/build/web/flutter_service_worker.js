403f83ae85aac14acf7d5b3487c418fc9db93757
Mahboob Hasan <mahboobhasan@Mahboobs-MacBook-Pro.local>
2026-08-26 15:38:12 +0530

    fix: add service worker cache invalidation script to force mobile browsers to update to latest build

--- flutter_app/build/web/flutter_service_worker.js
+++ flutter_app/build/web/flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "c27eeb72f2f6e8f285c9842d9b444772",
+const RESOURCES = {"flutter_bootstrap.js": "b16b1e2d961df1b57ee646a6bf006cab",
 "version.json": "7e6bcb3a73bf6b9b05e77f2ef1b8391d",
-"index.html": "0e4c3294217b8ef46b67166e0206ce90",
-"/": "0e4c3294217b8ef46b67166e0206ce90",
-"main.dart.js": "90ad8be70a6b503b1621cb3e8547cedd",
+"index.html": "8d30659e577350f2771dc0b659d700da",
+"/": "8d30659e577350f2771dc0b659d700da",
+"main.dart.js": "bf468c64a8a3eaf19d12a8506fc03efa",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
